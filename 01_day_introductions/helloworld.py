9a8ef152f6c9c484f4efc61ed53d6d331569ddeb
aqsyalraihanjamil <aqsyal_raihan_28rpl@student.smktelkom-mlg.sch.id>
2021-04-09 08:52:45 +0700

    first commit

---
+++ 01_day_introductions/helloworld.py
@@ -0,0 +1,41 @@
+# Exercises day 1
+import sys
+print("Version: " + sys.version + "\nInfo Version: ")
+print(sys.version_info)
+print()
+
+print(2 + 3)
+print(2 - 3)
+print(2 * 3)
+print(3 / 2)
+print(3 ** 2)
+print(3 % 2)
+print(5 // 2 ) # Ini operator untuk mendapatkan hasil bagi tapi tanpa koma
+print()
+
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(1 + 3j))      # Complex number
+print(type('Asabeneh'))  # String
+print(type([1, 2, 3]))   # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+print()
+
+print("Aqsyal Raihan Jamil")
+print("SEDULUR PARE")
+print("Your Country")
+print("I am enjoying 30 days of python\n")
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(["AN","USA","ASE"]))
+print(type("Aqsyal Raihan Jamil"))
+
+
+
+
+
