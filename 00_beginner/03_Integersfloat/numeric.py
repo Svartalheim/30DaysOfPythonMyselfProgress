aa2fa835c5ea28f5d9000bf9cfd65e0288855c73
Svartalheim <gboomers313@gmail.com>
2021-06-25 17:20:15 +0700

    Working with numeric data with explanation

---
+++ 00_beginner/03_Integersfloat/numeric.py
@@ -0,0 +1,23 @@
+# Arithmetic Operator
+# Addition:       3 + 2
+# Subtraction:    3 - 2
+# Multiplication: 3 * 2
+# Division:       3 / 2 (this give 1.5, for non comma, use floor division)
+# Floor division: 3 // 2
+# Exponent:       3 ** 2
+# Modulus:        3 % 2
+num1 = 3
+num2 = 4
+absolute = abs(num1+num2)
+# abs for absolute
+round = round(3.65, 1)
+#this will shown 3.7
+
+#Comparisons
+# Equal:              3 == 2
+# Not equal:          3 != 2
+# Greater than:       3 > 2
+# Less than:          3 < 2
+# Greater of equal:   3 >= 2
+# Less or equal:      3 <= 2
+
